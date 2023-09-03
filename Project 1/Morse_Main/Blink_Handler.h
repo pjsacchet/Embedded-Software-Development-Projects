9427305b0f442fb1e273547a8fe0ba260242ca08
rwalsh15 <rwalsh15@jhu.edu>
2023-09-03 15:17:42 -0400

    First go at making and ouput blink sequence. Note that the compiler is throwing errors at me for duplicate cases in the switch statement but I had to log off before I could find out why!

---
+++ Project 1/Morse_Main/Blink_Handler.h
@@ -0,0 +1,8 @@
+const int DOT = 1000; // one second
+const int DASH = 3000; // three seconds
+const int SAME_LETTER_SPACE = 1000; // one second rest while in same letter
+const int LETTER_SPACE = 3000; // three seconds between letters
+const int WORD_SPACE = 7000; // seven seconds between words
+
+
+void blinkLed(int* morseArray);
