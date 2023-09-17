780f67206b6a8dfd0fbcdb0e98d96697aa09925f
Patrick Sacchet <patrick.j.sacchet@gmail.com>
2023-09-17 14:19:04 -0400

    Adding base functionality for temp sensors and serial output

---
+++ Project 2/Temp_Main/Temp_Main.ino
@@ -0,0 +1,66 @@
+/**
+Project 2 - Serial Transmit of Temperature
+Patrick Sacchet, Robert Walsh
+EN.605.715.81.FA23
+Professor Doug Ferguson
+24SEP23
+*/
+
+// This file will serve for executing 'main' looping functionality, i.e., initializing the sensor, reading temparatures from the sensor, and outputtin them via serial
+  // PLEASE NOTE: funcitonality relies on the 'DHT sensor library' by Adafruit being installed on the running system prior to initialization
+  // REQUIRES the following Arduino libraries:
+  // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+  // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait ten seconds between measurements
+  delay(10000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
