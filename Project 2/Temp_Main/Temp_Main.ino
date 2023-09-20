d3c2fb90867e9c5ee778fa2ce6e471862e62b609
Patrick Sacchet <patrick.j.sacchet@gmail.com>
2023-09-20 16:08:35 -0400

    Modify serial output so we only display temp; print newline so we can format in csv easily

--- Project 2/Temp_Main/Temp_Main.ino
+++ Project 2/Temp_Main/Temp_Main.ino
@@ -52,15 +52,15 @@ void loop() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
+  //Serial.print(F("Humidity: "));
+  //Serial.print(h);
+  //Serial.print(F("%  Temperature: "));
+  //Serial.print(t);
+  //Serial.print(F("°C "));
+  Serial.println(f);
+  //Serial.print(F("°F  Heat index: "));
+  //Serial.print(hic);
+  //Serial.print(F("°C "));
+  //Serial.print(hif);
+  //Serial.println(F("°F"));
 }
