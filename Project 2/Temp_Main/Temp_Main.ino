f5d0975e166514150856bb87227c2c0ab9fd77e8
Patrick Sacchet <patrick.j.sacchet@gmail.com>
2023-09-21 20:10:31 -0400

    Removed old comments and readings from sensor

--- Project 2/Temp_Main/Temp_Main.ino
+++ Project 2/Temp_Main/Temp_Main.ino
@@ -63,11 +63,6 @@ void setup() {
 void loop() {
 
   float f = 0;
-  float h = 0;
-  float t = 0;
-
-  // Wait ten seconds between measurements
-  // delay(10000);
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -75,37 +70,16 @@ void loop() {
   if(count == 10)
   {
     f = dht.readTemperature(true);
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(f)) 
+    {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+
     Serial.print("Temperature = ");
     Serial.print(f);
     Serial.println("°F");
     count = 0;
   }
-  h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  // float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  //Serial.print(F("Humidity: "));
-  //Serial.print(h);
-  //Serial.print(F("%  Temperature: "));
-  //Serial.print(t);
-  //Serial.print(F("°C "));
-  //Serial.println(f);
-  //Serial.print(F("°F  Heat index: "));
-  //Serial.print(hic);
-  //Serial.print(F("°C "));
-  //Serial.print(hif);
-  //Serial.println(F("°F"));
 }
