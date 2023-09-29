e6425700622b58880b599715d5ced981e440853b
Patrick Sacchet <patrick.j.sacchet@gmail.com>
2023-09-29 09:31:35 -0400

    RPM measuring functionality working; code cleanup

--- Project 3/IR_Main.ino/IR_Main.ino.ino
+++ Project 3/IR_Main.ino/IR_Main.ino.ino
@@ -1,18 +1,57 @@
 int pd=2; //Photodiode to digital pin 2
-int buzz=13; //piezo buzzer to digital pin 13 
 int senRead=0; //Readings from sensor to analog pin 0 
-int limit=850; //Threshold range of an obstacle 
+int limit=650; //Threshold range of an obstacle 
+
+volatile byte rpmCount;
+volatile int status;
+
+unsigned int rpm;
+
+unsigned long timeOld;
+
+void rpm_fun()
+{
+  // Each rotation, this interrupt is run twice
+  // Update count (really just need this, not a flashy light)
+  rpmCount++;
+}
+
+
 void setup() 
 { 
-  pinMode(pd,OUTPUT); 
-  pinMode(buzz,OUTPUT);
-  digitalWrite(pd,HIGH); //supply 5 volts to photodiode 
-  //digitalWrite(buzz,LOW); //set the buzzer in off mode (initial condition) 
-  Serial.begin(9600); //setting serial monitor at a default baund rate of 9600 
-  } 
+  // Setting serial monitor at a default baund rate of 9600 
+  Serial.begin(9600); 
+
+  // Actually only want this to fire when digital pin 2 rises 
+  attachInterrupt(0, rpm_fun, RISING); 
+  
+  // Values for calculating RPM
+  rpm = 0;
+  rpmCount = 0;
+  timeOld = 0;
+  status = LOW;
+  
+} 
   
 void loop() 
 { 
+  delay(1000);
+
+  // Dont process interrupts during calculations
+  detachInterrupt(0);
+
+  // We use 20 here since we have three propellers on our 'fan' (60/3)
+  rpm = 20*1000/(millis() - timeOld)*rpmCount;
+  timeOld = millis();
+  rpmCount = 0;
+
+  Serial.println(rpm, DEC);
+ 
+  // restart interrupt
+  attachInterrupt(0, rpm_fun, RISING);
+
+  // Previous code I used for threashold handling 
+  /**
   int val=analogRead(senRead); //variable to store values from the photodiode 
   Serial.println(val); // prints the values from the sensor in serial monitor 
   if(val <= limit) //If obstacle is nearer than the Threshold range 
@@ -27,4 +66,8 @@ void loop()
     Serial.println("Value is greater than limit!");
     delay(1000); 
   } 
+  */
 }
+
+
+ 
