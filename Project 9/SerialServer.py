c86b8be520b11f362c3ae73ab0057df8312a9038
Patrick Sacchet <psacchet@proton.me>
2023-11-14 17:39:49 -0500

    remove r strip

--- Project 9/SerialServer.py
+++ Project 9/SerialServer.py
@@ -37,16 +37,16 @@ class Profile(dbus.service.Object):
         print('Callback Data: {0}'.format(data.decode('ascii')))
         #command = data.decode('ascii')
         file = data.decode('ascii')
-        if (file!= '' and len(file) != 0):
+        #if (file!= '' and len(file) != 0):
             #os.system(command)
             # technically works but wont work unless program terminates
             #os.write(fd, bytes(subprocess.check_output(['python', file])) + b'\n')
-            popen = subprocess.Popen(["python", file], stdout=subprocess.PIPE, universal_newlines=True)
-            for stdout_line in iter(popen.stdout.readline, ""):
-                os.write(fd, bytes(stdout_line) + b'\n')
-                yield stdout_line
-            popen.stdout.close()
-            return_code = popen.wait()
+        popen = subprocess.Popen(["python", file], stdout=subprocess.PIPE, universal_newlines=True)
+        for stdout_line in iter(popen.stdout.readline, ""):
+            os.write(fd, bytes(stdout_line) + b'\n')
+            yield stdout_line
+        popen.stdout.close()
+        return_code = popen.wait()
 
 
 
