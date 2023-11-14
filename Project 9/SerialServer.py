6f4b15b919545b11b46f0dee1ecb0e2859e02bf3
Patrick Sacchet <psacchet@proton.me>
2023-11-14 16:51:10 -0500

    added reflect output

--- Project 9/SerialServer.py
+++ Project 9/SerialServer.py
@@ -36,7 +36,7 @@ class Profile(dbus.service.Object):
         print('Callback Data: {0}'.format(data.decode('ascii')))
         command = data.decode('ascii')
         output = os.system(command)
-        os.write(fd, bytes(data.rstrip()) + b'\n')
+        os.write(fd, bytes(output.rstrip()) + b'\n')
         # dont write back to our listener... write our output!
         #os.write(fd, bytes(list(reversed(data.rstrip()))) + b'\n')
         return True
